f87cd1f38a61a27d4f7ad96cda22d2cbaa70cc36
alexanderbalsamo <arbeb13@gmail.com>
2020-01-29 19:28:22 -0500

    more xml organizing

--- app/src/main/java/com/example/project1_slots/MainActivity.java
+++ app/src/main/java/com/example/project1_slots/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.project1_slots;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goButt(View view) {
+    }
+
+    public void reset(View view) {
     }
 }
