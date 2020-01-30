52f25d9e23309f5bde5f2b8979ebd986239b74b4
alexanderbalsamo <arbeb13@gmail.com>
2020-01-30 14:49:06 -0500

    added wallet code on startup that replaces the textView with the correct amount of money

--- app/src/main/java/com/example/project1_slots/MainActivity.java
+++ app/src/main/java/com/example/project1_slots/MainActivity.java
@@ -3,19 +3,35 @@ package com.example.project1_slots;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.project1_slots.CONSTANTS;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Balsamo";
+
+    // Dealing with Money
+    TextView wallet;
+    int startupCash = CONSTANTS.STARTUP_CASH;
+    int money = startupCash;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Start up Money
+        wallet = (TextView)findViewById(R.id.money);
+        wallet.setText(String.format("$%s", Integer.toString(startupCash)));
+        Log.d(TAG, "Money you have on startUp should = 5: " + Integer.toString(money)); //debug statement
     }
 
     public void goButt(View view) {
     }
 
-    public void reset(View view) {
+    public void resetButt(View view) {
     }
 }
